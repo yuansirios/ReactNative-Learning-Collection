d2d68d7338230c97af6358db5347a6442f74d9ac
yuan <695580111@qq.com>
2019-07-29 17:21:24 +0800

    导航栏添加、列表创建

--- app/InitApp.js
+++ app/InitApp.js
@@ -1,11 +1,25 @@
 
 import React from "react";
-import HomeList from './Pages/HomeList';
+import {
+  View,
+  Text
+} from 'react-native';
+import HomeList from './Pages/HomeList'
+
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeList
+  }
+});
+
+const AppContainer = createAppContainer(AppNavigator)
 
 export default class InitApp extends React.Component {
   render() {
     return (
-      <HomeList />//createAppContainer(AppNavigator)
+      <AppContainer/>
     )
   }
 }
--- app/pages/HomeList.js
+++ app/pages/HomeList.js
@@ -1,12 +1,55 @@
 import React, { Component } from "react";
-import {View, Text} from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+class HomeCell extends Component {
+  render() {
+    return (
+      <View style={styles.homeCell}>
+        <Text>{this.props.value}</Text>
+      </View>
+    );
+  }
+}
+
+const listArr = [
+  { key: '1', component: '123' },
+  { key: '2', component: '123' },
+  { key: '3', component: '123' },
+  { key: '4', component: '123' },
+  { key: '5', component: '123' },
+  { key: '6', component: '123' },
+  { key: '7', component: '123' },
+  { key: '8', component: '123' },
+  { key: '9', component: '123' },
+  { key: '10', component: '123' },
+];
 
 export default class HomeList extends Component {
-    render() {
-        return (
-          <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>HomeList1</Text>
-          </View>
-        );
-      }
+  render() {
+    return (
+      <View style={styles.homeList}>
+        <FlatList
+          data={listArr}
+          renderItem={({ item }) =>
+            <HomeCell value={item.key} />
+          }
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  homeList: {
+    width: "auto",
+    height: 400,
+    backgroundColor: "#129999"
+  },
+  homeCell: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomColor: "#123123",
+    borderBottomWidth: 1
+  }
+});
