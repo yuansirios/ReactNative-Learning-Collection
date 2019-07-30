3c08570105a21b82e6372c7f78dfc1b430ee35af
袁晓栋 <yuanxd25114@hsjry.com>
2019-07-30 20:57:18 +0800

    FlatList示例完善

--- app/InitApp.js
+++ app/InitApp.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React from 'react';
+import { Text } from 'react-native';
 import HomeList from './Pages/HomeList'
 import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class WelcomePage extends React.Component {
+  render() {
+    return (
+      <Text>Welcome</Text>
+    )
+  }
+}
 
 const AppNavigator = createStackNavigator({
   Home: {
     screen: HomeList
+  },
+  Welcome:{
+    screen:WelcomePage
   }
 });
 
--- app/pages/HomeList.js
+++ app/pages/HomeList.js
@@ -1,55 +1,98 @@
+
 import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-class HomeCell extends Component {
-  render() {
-    return (
-      <View style={styles.homeCell}>
-        <Text>{this.props.value}</Text>
-      </View>
-    );
-  }
-}
-
-const listArr = [
-  { key: '1', component: '123' },
-  { key: '2', component: '123' },
-  { key: '3', component: '123' },
-  { key: '4', component: '123' },
-  { key: '5', component: '123' },
-  { key: '6', component: '123' },
-  { key: '7', component: '123' },
-  { key: '8', component: '123' },
-  { key: '9', component: '123' },
-  { key: '10', component: '123' },
-];
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 export default class HomeList extends Component {
+  static navigationOptions = {
+    title: 'ReactNative',
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [
+        { key: '1、导航跳转-常用设置', component: 'Welcome' },
+        { key: '2、FlatList详细使用', component: '123' },
+        { key: '3', component: '123' },
+        { key: '4', component: '123' },
+        { key: '5', component: '123' },
+        { key: '6', component: '123' },
+        { key: '7', component: '123' },
+        { key: '8', component: '123' },
+        { key: '9', component: '123' },
+        { key: '10', component: '123' },
+      ],
+    };
+  }
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View style={styles.homeList}>
         <FlatList
-          data={listArr}
-          renderItem={({ item }) =>
-            <HomeCell value={item.key} />
-          }
+          data={this.state.dataSource}
+          renderItem={({ item }) => this._createListItem(item)}
+          ItemSeparatorComponent={this._separator}
         />
       </View>
     );
   }
+
+  /**
+   * cell
+   *
+   * @param {*} item
+   * @returns
+   * @memberof HomeList
+   */
+  _createListItem(item) {
+    return (
+      <TouchableOpacity activeOpacity={0.5} onPress={() => this._onItemClick(item)}>
+        <View style={styles.homeCell}>
+          <Text style={{color:'white'}}>{item.key}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  /**
+   *  分割线
+   *  
+   * @returns
+   * @memberof HomeList
+   */
+  _separator() {
+    return <View style={{ height: 1, backgroundColor: '#999999' }} />;
+  }
+
+  /**
+   * item点击事件
+   *
+   * @param {*} item
+   * @memberof HomeList
+   */
+  _onItemClick(item) {
+    console.log("点击了："+item.key);
+    const {navigate} = this.props.navigation;
+    navigate(item.component);
+  }
+
 }
 
 const styles = StyleSheet.create({
   homeList: {
-    width: "auto",
-    height: 400,
-    backgroundColor: "#129999"
+    backgroundColor: "#998877"
   },
   homeCell: {
     height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    borderBottomColor: "#123123",
-    borderBottomWidth: 1
+    marginLeft:20,
+    justifyContent: "center"
   }
 });
