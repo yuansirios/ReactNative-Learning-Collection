f3335ac51a7ed801f121f8983e372b535cf6e3a8
袁晓栋 <yuanxd25114@hsjry.com>
2019-07-30 19:36:54 +0800

    代码调试

--- app/pages/HomeList.js
+++ app/pages/HomeList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 class HomeCell extends Component {
